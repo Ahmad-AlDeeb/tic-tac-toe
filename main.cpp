6a1cb128b5d5a1d751a918f34d21497003fca64d
Ahmad Al-Deeb <ahmeddeeb22222@gmail.com>
2024-02-23 20:54:56 +0200

    Implement display_scores()

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ const int grid_cols = 13;
 char grid[grid_rows][grid_cols];
 bool end_game {false};
 int turns {0};
-int player1_score {0};
-int player2_score {0};
+int player_O_score {0};
+int player_X_score {0};
 int winner;
 map<int,int> get_row_index;
 map<int,int> get_col_index;
@@ -64,13 +64,16 @@ void build_grid() {
 
 void refresh() {
 //    clear_console();
-//    display_scores();
+    display_scores();
     render_grid();
 }
 
 void clear_console() {}
 
-void display_scores() {}
+void display_scores() {
+    cout << "Player O Score: " << player_O_score << endl;
+    cout << "Player X Score: " << player_X_score << endl;
+}
 
 void render_grid() {
     for(int i{}; i<grid_rows; i++) {
